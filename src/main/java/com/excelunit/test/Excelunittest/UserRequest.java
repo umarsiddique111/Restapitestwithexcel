3663eade19c55f006ae5fa907883a265f9dd3cbb
umarsiddique111 <39054374+umarsiddique111@users.noreply.github.com>
2018-06-06 17:45:59 +0500

    User request upload file

---
+++ src/main/java/com/excelunit/test/Excelunittest/UserRequest.java
@@ -0,0 +1,47 @@
+package com.excelunit.test.Excelunittest;
+
+public class UserRequest {
+	
+	private String url;
+	private String action;
+	private String parameter;
+	private String conten_type;
+	private String status;
+	
+	
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
+	}
+	public String getParameter() {
+		return parameter;
+	}
+	public void setParameter(String parameter) {
+		this.parameter = parameter;
+	}
+	public String getConten_type() {
+		return conten_type;
+	}
+	public void setConten_type(String conten_type) {
+		this.conten_type = conten_type;
+	}
+
+	
+	
+	
+}
