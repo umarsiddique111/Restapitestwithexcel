3c85fa175086c6ab49321c39d697a95b0f720a6d
umarsiddique111 <39054374+umarsiddique111@users.noreply.github.com>
2018-06-06 17:44:19 +0500

    update test case

--- src/test/java/com/excelunit/test/Excelunittest/ExcelunittestApplicationTests.java
+++ src/test/java/com/excelunit/test/Excelunittest/ExcelunittestApplicationTests.java
@@ -1,23 +1,25 @@
 package com.excelunit.test.Excelunittest;
 
 
+
 import static org.junit.Assert.assertEquals;
 
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
 
+import jxl.Cell;
 import jxl.Sheet;
+import jxl.Workbook;
 import jxl.read.biff.BiffException;
 
 @RunWith(SpringRunner.class)
@@ -27,55 +29,84 @@ public class ExcelunittestApplicationTests {
 	
 	private final static Logger logger = Logger.getLogger(ExcelunittestApplication.class.getName());
 	 
-	
+	private final static String FilePath ="D:\\detail.xls";
 	@Test 
-	public void postapitest() throws BiffException, IOException {
-	    String FilePath ="D:\\detail.xls";
-	    ReadExcelFile readExcelFile = new ReadExcelFile();
-		Sheet resd = readExcelFile.readExcel(FilePath);
-		String url = resd.getCell(1, 1).getContents();
-		String parametr = resd.getCell(3, 1).getContents();
-		Stringsplitmethod stringsplitmethod = new Stringsplitmethod();
-		stringsplitmethod.splitstr(parametr);
-		String[] array = stringsplitmethod.splitstr(parametr);
+	public void postapitest() throws IOException, BiffException  {
+	   
 	
-MultiValueMap<String, Object> bodyMap = new LinkedMultiValueMap<>();
-        File file = new File(array[0]);
-        bodyMap.add("user-file",new FileSystemResource(file));
-        bodyMap.add("asOfDate",array[1]);
-        bodyMap.add("name",array[2]);
 		
-		UploadClient uploadClient = new UploadClient();
-		String result = uploadClient.uploadfile(url,bodyMap);
-		
-		logger.log(Level.INFO, "check reuslt"+result);
-		assertEquals("File uploaded succefully", result);
+		FileInputStream fs = new FileInputStream(FilePath);
+		Workbook wb = Workbook.getWorkbook(fs);
 
-}		
-	@Test
-	public void getapitestparam() throws BiffException, IOException {
-		String FilePath ="D:\\detail.xls";
-		ReadExcelFile readExcelFile1 = new ReadExcelFile();
-		Sheet res = readExcelFile1.readExcel(FilePath);
-		String url = res.getCell(1, 2).getContents();
-		RestTemplate resttemplate = new RestTemplate();
-		String name = "saleem";
-		String result1 = resttemplate.getForObject(url, String.class, name);
-		logger.log(Level.INFO, "check getapitestparam reuslt"+result1);
-		assertEquals("get succeful run parameter!!!"+name, result1);
+		// TO get the access to the sheet
+		Sheet sheet = wb.getSheet("Sheet1");
+	
+		 MultiValueMap<String, Object> bodyMap = new LinkedMultiValueMap<>();
+		 List<UserRequest> userlist = new ArrayList<UserRequest>();
+		
+		 System.out.println("-----------------------"+sheet.getRows());
+		 for (int row = 2; row < sheet.getRows(); row++) {
+			 Cell[] cells = sheet.getRow(row);
+			
+					System.out.println(cells[1].getContents());
+					System.out.println(cells[2].getContents());
+					System.out.println(cells[3].getContents());
+					System.out.println(cells[4].getContents());
+					
+					
+					UserRequest request = new UserRequest();
+					
+							request.setUrl(cells[1].getContents());
+							request.setAction(cells[2].getContents());
+							request.setParameter(cells[3].getContents());
+							request.setConten_type(cells[4].getContents());
+							
+							userlist.add(request);
+							
+							String url = request.getUrl();
+							String Action = request.getAction();
+							String content_type = request.getConten_type();
+							String Parameter = request.getParameter();
+							String[] param = Parameter.split(",");
+							
+							System.out.println("i am here at 1");
+							for (int i =0;i<param.length; i++) {					
+								 String param2 = param[i];
+								 
+								 if(param2.isEmpty()) {
+									 continue;
+								 }
+								 System.out.println("i am here at 2");
+								 System.out.println("parameter lenth"+param.length);
+								 System.out.println("this is Split paramter"+ param2);
+								 String[] param12 = param2.split("=");
+								 String key = param12[0];
+								 String value = param12[1];
+								
+									
+							     
+							       
+							
+
+									
+									
+								  bodyMap.add(key,value);
+							}
+							     
+						        UploadClient uploadClient = new UploadClient();
+								String result = uploadClient.uploadfile(url,bodyMap,Action,content_type);
+								System.out.println("body send "+bodyMap);
+								System.out.println("My final result "+" "+result);
+		 }
+		 
+
+		 
+		assertEquals("succefully", "succefully");
+		
+	
+		
+		
 		
 	}
-	@Test 
-	public void getapitest() throws BiffException, IOException {
-		String FilePath ="D:\\detail.xls";
-		ReadExcelFile readExcelFile1 = new ReadExcelFile();
-		Sheet res = readExcelFile1.readExcel(FilePath);
-		String url = res.getCell(1, 3).getContents();
-		RestTemplate resttemplate = new RestTemplate();
-		String result2 = resttemplate.getForObject(url, String.class);
-		logger.log(Level.INFO, "check getapitest result"+result2);
-		assertEquals("Check Get Controller", result2);
-	}
-		
-
-}
+			
+		}
